ae5b5f2dcac293ce4017e0530ce22ee621eed6a4
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-01-23 04:08:04 -0300

    style(table): make table scrollable on smaller screens

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,4 +5,18 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## 0.1.0
+## [Unreleased](https://github.com/henriquehbr/easycarros-challenge/compare/v0.1.0...HEAD)
+
+### Commits
+
+- style(table): make table scrollable on smaller screens [`b923c9f`](https://github.com/henriquehbr/easycarros-challenge/commit/b923c9f4c243f1bd5ba26d837ed611fb43b83861)
+
+## v0.1.0 - 2021-01-23
+
+### Commits
+
+- feat(pages): create homepage [`bae004b`](https://github.com/henriquehbr/easycarros-challenge/commit/bae004bb9968c2b2fe47725059465ca1ef77e79b)
+- Initial commit [`fa8adf5`](https://github.com/henriquehbr/easycarros-challenge/commit/fa8adf5bf0f054d01a51a643032ec82c294cde46)
+- feat(pages): create CRUD page [`a4571eb`](https://github.com/henriquehbr/easycarros-challenge/commit/a4571eb2e8d18aa884feba410ecd786443d7d429)
+- chore: prevent npm publish after changelog generation [`8896b2f`](https://github.com/henriquehbr/easycarros-challenge/commit/8896b2f93a5ba7c8170a50c395c2427478ff71a5)
+- chore: remove unneded test script [`cd13527`](https://github.com/henriquehbr/easycarros-challenge/commit/cd13527b442d4f6ba9499342dc1ab0178b7ab46e)
--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -5,8 +5,13 @@
 </script>
 
 <style>
+  .table-container {
+    overflow: auto;
+    max-width: 1024px;
+    width: 100%;
+  }
+
   table {
-    border-spacing: 0;
     border-spacing: 0 1em;
   }
 
@@ -55,62 +60,64 @@
   }
 </style>
 
-<table>
-  <thead>
-    <td>Serviço</td>
-    <td>Data de execução</td>
-    <td>Data de Agendamento</td>
-    <td>Placa</td>
-    <td></td>
-  </thead>
-  <tbody>
-    <tr>
-      <td class='service-name'>Lavagem ecológica</td>
-      <td>---</td>
-      <td>20/08/2020</td>
-      <td class='vehicle-plate'>MBB-1996</td>
-      <td class='action-buttons'>
-        <Button small secondary color='red'>
-          <img src={exitIcon} alt='Exit icon'>
-          Excluir
-        </Button>
-        <Button small secondary color='limegreen'>
-          <img src={checkIcon} alt='Check icon' />
-          Finalizar
-        </Button>
-      </td>
-    </tr>
-    <tr>
-      <td class='service-name'>Martelinho de Ouro</td>
-      <td>04/08/2020</td>
-      <td>04/08/2020</td>
-      <td class='vehicle-plate'>MAR-1993</td>
-      <td class='action-buttons'>
-        <Button small secondary color='red'>
-          <img src={exitIcon} alt='Exit icon'>
-          Excluir
-        </Button>
-        <Button small secondary color='limegreen'>
-          <img src={checkIcon} alt='Check icon' />
-          Finalizar
-        </Button>
-      </td>
-    </tr>
-    <tr>
-      <td class='service-name'>Higienização completa</td>
-      <td>30/07/2020</td>
-      <td>30/07/2020</td>
-      <td class='vehicle-plate'>URL-2020</td>
-      <td class='action-buttons'>
-        <Button small secondary color='red'>
-          <img src={exitIcon} alt='Exit icon'>
-          Excluir
-        </Button>
-        <Button small secondary color='limegreen'>
-          <img src={checkIcon} alt='Check icon' />
-          Finalizar
-        </Button>
-      </td>
-    </tr>
-  </tbody>
-</table>
+<div class='table-container'>
+  <table>
+    <thead>
+      <td>Serviço</td>
+      <td>Data de execução</td>
+      <td>Data de Agendamento</td>
+      <td>Placa</td>
+      <td></td>
+    </thead>
+    <tbody>
+      <tr>
+        <td class='service-name'>Lavagem ecológica</td>
+        <td>---</td>
+        <td>20/08/2020</td>
+        <td class='vehicle-plate'>MBB-1996</td>
+        <td class='action-buttons'>
+          <Button small secondary color='red'>
+            <img src={exitIcon} alt='Exit icon'>
+            Excluir
+          </Button>
+          <Button small secondary color='limegreen'>
+            <img src={checkIcon} alt='Check icon' />
+            Finalizar
+          </Button>
+        </td>
+      </tr>
+      <tr>
+        <td class='service-name'>Martelinho de Ouro</td>
+        <td>04/08/2020</td>
+        <td>04/08/2020</td>
+        <td class='vehicle-plate'>MAR-1993</td>
+        <td class='action-buttons'>
+          <Button small secondary color='red'>
+            <img src={exitIcon} alt='Exit icon'>
+            Excluir
+          </Button>
+          <Button small secondary color='limegreen'>
+            <img src={checkIcon} alt='Check icon' />
+            Finalizar
+          </Button>
+        </td>
+      </tr>
+      <tr>
+        <td class='service-name'>Higienização completa</td>
+        <td>30/07/2020</td>
+        <td>30/07/2020</td>
+        <td class='vehicle-plate'>URL-2020</td>
+        <td class='action-buttons'>
+          <Button small secondary color='red'>
+            <img src={exitIcon} alt='Exit icon'>
+            Excluir
+          </Button>
+          <Button small secondary color='limegreen'>
+            <img src={checkIcon} alt='Check icon' />
+            Finalizar
+          </Button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
